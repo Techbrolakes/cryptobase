563838774f23abafeb26a4557e155bd85861b3c1
Techbrolakes <lekandar11@gmail.com>
2023-01-24 22:05:10 +0100

    Feat: Spinner Font Size Configurations

--- components/common/Spinner/Index.tsx
+++ components/common/Spinner/Index.tsx
@@ -1,4 +1,3 @@
-import { Text } from '@chakra-ui/react';
 import React from 'react';
 import Lottie from 'lottie-react';
 import bitcoin from '@/animations/bitcoin.json';
@@ -7,7 +6,7 @@ const Spinner: React.FC = () => {
     return (
         <div className="min-h-screen flex flex-col space-y-2 justify-center items-center">
             <Lottie animationData={bitcoin} className="w-48" />
-            <Text as={'p'}>Loading</Text>
+            <p className="cb-text">Loading</p>
         </div>
     );
 };
