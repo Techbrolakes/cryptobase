cea614e2d9711dee2e7da78a26e30f997c441203
Techbrolakes <lekandar11@gmail.com>
2023-01-24 18:52:52 +0100

    Feat: Website Animations and Coins Page Configs

--- components/blocks/coins/Coin.tsx
+++ components/blocks/coins/Coin.tsx
@@ -1,22 +1,29 @@
 import React, { useState } from 'react';
 import { useGetCryptosQuery } from '@/services/CoinRankingApi';
 import Spinner from '@/common/Spinner/Index';
+import Lottie from 'lottie-react';
+import bitcoin from '@/animations/bitcoin.json';
 import CoinCard from '@/common/CoinCard';
 import { Button, Input } from '@chakra-ui/react';
 
 const Coin: React.FC = () => {
-    const { data, isFetching } = useGetCryptosQuery(100);
+    const { data, isFetching } = useGetCryptosQuery(1000);
     const coinPerRow = 9;
     const [next, setNext] = useState(coinPerRow);
+    const [loading, setLoading] = useState(false);
+    const coins = data?.data?.coins;
 
     const handleLoadMore = () => {
-        setNext(next + coinPerRow);
+        setLoading(true);
+        setTimeout(() => {
+            setNext(next + coinPerRow);
+            setLoading(false);
+        }, 3000);
     };
-    const coins = data?.data?.coins;
 
     if (isFetching) return <Spinner />;
     return (
-        <section className="p-3 lg:p-6 space-y-12 mb-20">
+        <section className="p-3 lg:p-6 space-y-4 mb-20 lg:mb-20">
             <div className="w-2/3 lg:w-1/2 mx-auto">
                 <Input name="" variant="filled" placeholder="Search Cryptocurrency" />
             </div>
@@ -25,8 +32,9 @@ const Coin: React.FC = () => {
                     <CoinCard key={coin.uuid} {...coin} />
                 ))}
             </div>
+            <div className="flex justify-center">{loading && <Lottie animationData={bitcoin} className="w-24" />}</div>{' '}
             {next < coins?.length && (
-                <div className="flex justify-center mt-8">
+                <div className="flex justify-center">
                     <Button onClick={handleLoadMore}>Load More Coins</Button>
                 </div>
             )}
--- components/common/Spinner/Index.tsx
+++ components/common/Spinner/Index.tsx
@@ -1,10 +1,12 @@
-import { CircularProgress, Text } from '@chakra-ui/react';
+import { Text } from '@chakra-ui/react';
 import React from 'react';
+import Lottie from 'lottie-react';
+import bitcoin from '@/animations/bitcoin.json';
 
 const Spinner: React.FC = () => {
     return (
         <div className="min-h-screen flex flex-col space-y-2 justify-center items-center">
-            <CircularProgress isIndeterminate color="blue.300" />
+            <Lottie animationData={bitcoin} className="w-48" />
             <Text as={'p'}>Loading</Text>
         </div>
     );
