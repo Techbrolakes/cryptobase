01691b8eb0f14d15469e5dec5cf645a489e57b35
Techbrolakes <lekandar11@gmail.com>
2023-01-28 00:44:17 +0100

    Feat: Removing the line chart Codes

--- components/blocks/coins/SingleCoinDetails.tsx
+++ components/blocks/coins/SingleCoinDetails.tsx
@@ -4,18 +4,18 @@ import { IGetCryptoDetails } from '@/services/interface';
 import { Avatar } from '@chakra-ui/react';
 import { useGetCryptosPriceHistoryQuery } from '@/services/CoinRankingApi';
 import Spinner from '@/common/Spinner/Index';
-import { Line } from 'react-chartjs-2';
-import {
-    Chart as ChartJS,
-    CategoryScale,
-    LinearScale,
-    PointElement,
-    LineElement,
-    Title,
-    Tooltip,
-    Legend,
-} from 'chart.js';
-ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend);
+// import { Line } from 'react-chartjs-2';
+// import {
+//     Chart as ChartJS,
+//     CategoryScale,
+//     LinearScale,
+//     PointElement,
+//     LineElement,
+//     Title,
+//     Tooltip,
+//     Legend,
+// } from 'chart.js';
+// ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend);
 
 interface IProps {
     data: IGetCryptoDetails;
@@ -27,39 +27,39 @@ const SingleCoinDetails: React.FC<IProps> = ({ data }: IProps) => {
     const coinPrice = [];
     const coinTimestamp = [];
 
-    // Getting Individual Prices
-    for (let i = 0; i < historyData?.data?.history?.length; i++) {
-        coinPrice.push(historyData?.data?.history[i].price);
-    }
+    // // Getting Individual Prices
+    // for (let i = 0; i < historyData?.data?.history?.length; i++) {
+    //     coinPrice.push(historyData?.data?.history[i].price);
+    // }
 
-    // converting the timestamp to date
-    for (let i = 0; i < historyData?.data?.history?.length; i++) {
-        coinTimestamp.push(new Date(historyData?.data?.history[i].timestamp).toLocaleDateString());
-    }
+    // // converting the timestamp to date
+    // for (let i = 0; i < historyData?.data?.history?.length; i++) {
+    //     coinTimestamp.push(new Date(historyData?.data?.history[i].timestamp).toLocaleDateString());
+    // }
 
-    // Setting up the data
-    const historyInfo = {
-        datasets: [
-            {
-                backgroundColor: '#0071bd',
-                borderColor: '#0071bd',
-                data: coinPrice,
-                fill: false,
-                label: 'Price In USD',
-            },
-        ],
-        labels: coinTimestamp,
-    };
+    // // Setting up the data
+    // const historyInfo = {
+    //     datasets: [
+    //         {
+    //             backgroundColor: '#0071bd',
+    //             borderColor: '#0071bd',
+    //             data: coinPrice,
+    //             fill: false,
+    //             label: 'Price In USD',
+    //         },
+    //     ],
+    //     labels: coinTimestamp,
+    // };
 
-    // Setting up the options
-    const options = {
-        plugins: {
-            legend: {
-                position: 'top' as const,
-            },
-        },
-        responsive: true,
-    };
+    // // Setting up the options
+    // const options = {
+    //     plugins: {
+    //         legend: {
+    //             position: 'top' as const,
+    //         },
+    //     },
+    //     responsive: true,
+    // };
 
     if (isFetching) return <Spinner />;
     return (
@@ -77,9 +77,9 @@ const SingleCoinDetails: React.FC<IProps> = ({ data }: IProps) => {
                 </p>
             </section>
 
-            <div className="bg-white shadow-2xl rounded-lg">
+            {/* <div className="bg-white shadow-2xl rounded-lg">
                 <Line data={historyInfo} options={options} className="p-4" />
-            </div>
+            </div> */}
         </div>
     );
 };
