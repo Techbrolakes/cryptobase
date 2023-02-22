bcd1970e919f6b8b129277a28daf1b061841a88d
Techbrolakes <lekandar11@gmail.com>
2023-02-22 19:07:48 +0100

    feat: remove the charts configurations

--- components/blocks/coins/SingleCoinDetails.tsx
+++ components/blocks/coins/SingleCoinDetails.tsx
@@ -4,13 +4,11 @@ import { IGetCryptoDetails } from '@/services/interface';
 import { Avatar } from '@chakra-ui/react';
 import Spinner from '@/common/Spinner/Index';
 import HTMLReactParser from 'html-react-parser';
-import { useGetCryptosPriceHistoryQuery } from '@/services/CoinRankingApi';
-import dynamic from 'next/dynamic';
-const ApexCharts = dynamic(() => import('react-apexcharts'), { ssr: false });
-import { ApexOptions } from 'apexcharts';
+// import { useGetCryptosPriceHistoryQuery } from '@/services/CoinRankingApi';
+// import dynamic from 'next/dynamic';
+// const ApexCharts = dynamic(() => import('react-apexcharts'), { ssr: false });
+// import { ApexOptions } from 'apexcharts';
 import millify from 'millify';
-import dayjs from 'dayjs';
-import Loading from '@/common/Loading';
 import icons from '@/icons';
 import { getOrdinal } from '@/common/utils';
 
@@ -32,9 +30,9 @@ interface IProps {
 
 const SingleCoinDetails: React.FC<IProps> = ({ data }: IProps) => {
     const details = data?.data?.coin;
-    const { data: coinHistory, isFetching, isLoading } = useGetCryptosPriceHistoryQuery(details?.uuid);
-    const coinPrice = [];
-    const coinTimestamp = [];
+    // const { data: coinHistory, isFetching, isLoading } = useGetCryptosPriceHistoryQuery(details?.uuid);
+    // const coinPrice = [];
+    // const coinTimestamp = [];
 
     const STATS = [
         {
@@ -93,156 +91,156 @@ const SingleCoinDetails: React.FC<IProps> = ({ data }: IProps) => {
         },
     ];
 
-    for (let i = 0; i < coinHistory?.data?.history?.length; i += 1) {
-        coinPrice.push(
-            millify(coinHistory?.data?.history[i].price, {
-                precision: 3,
-            }),
-        );
-    }
+    // for (let i = 0; i < coinHistory?.data?.history?.length; i += 1) {
+    //     coinPrice.push(
+    //         millify(coinHistory?.data?.history[i].price, {
+    //             precision: 3,
+    //         }),
+    //     );
+    // }
 
-    for (let i = 0; i < coinHistory?.data?.history?.length; i += 1) {
-        coinTimestamp.push(
-            dayjs(new Date(coinHistory?.data?.history[i].timestamp).toLocaleDateString()).format('DD MMM YYYY'),
-        );
-    }
+    // for (let i = 0; i < coinHistory?.data?.history?.length; i += 1) {
+    //     coinTimestamp.push(
+    //         dayjs(new Date(coinHistory?.data?.history[i].timestamp).toLocaleDateString()).format('DD MMM YYYY'),
+    //     );
+    // }
 
-    const options: ApexOptions = {
-        chart: {
-            animations: {
-                animateGradually: {
-                    delay: 150,
-                    enabled: true,
-                },
-                dynamicAnimation: {
-                    enabled: true,
-                    speed: 350,
-                },
-                easing: 'easeinout',
-                enabled: true,
-                speed: 800,
-            },
-            background: '#fff',
-            height: 600,
-            zoom: {
-                enabled: true,
-            },
-        },
-        dataLabels: {
-            enabled: false,
-        },
-        fill: {
-            colors: ['#2E93fA', '#66DA26', '#546E7A', '#E91E63', '#FF9800'],
-        },
-        grid: {
-            borderColor: '#90A4AE',
-            column: {
-                colors: undefined,
-                opacity: 0.5,
-            },
-            padding: {
-                bottom: -10,
-                left: 10,
-                right: 10,
-                top: 0,
-            },
-            position: 'back',
-            row: {
-                colors: undefined,
-                opacity: 0.5,
-            },
-            show: true,
-            strokeDashArray: 0,
-            xaxis: {
-                lines: {
-                    show: false,
-                },
-            },
-            yaxis: {
-                lines: {
-                    show: false,
-                },
-            },
-        },
-        markers: {
-            colors: undefined,
-            discrete: [],
-            fillOpacity: 1,
-            hover: {
-                size: undefined,
-                sizeOffset: 3,
-            },
-            offsetX: 0,
-            offsetY: 0,
-            onClick: undefined,
-            onDblClick: undefined,
-            radius: 2,
-            shape: 'circle',
-            showNullDataPoints: true,
-            size: 0,
-            strokeColors: '#fff',
-            strokeDashArray: 0,
-            strokeOpacity: 0.9,
-            strokeWidth: 2,
-        },
-        stroke: {
-            curve: 'straight',
-        },
-        tooltip: {
-            custom: undefined,
-            enabled: true,
-            enabledOnSeries: undefined,
-            fillSeriesColor: false,
-            fixed: {
-                enabled: false,
-                offsetX: 0,
-                offsetY: 0,
-                position: 'topRight',
-            },
-            followCursor: false,
-            intersect: false,
-            inverseOrder: false,
-            items: {
-                display: 'flex',
-            },
-            marker: {
-                show: true,
-            },
-            onDatasetHover: {
-                highlightDataSeries: false,
-            },
-            shared: true,
-            style: {
-                fontFamily: undefined,
-                fontSize: '16px',
-            },
-            x: {
-                format: 'dd MMM',
-                formatter: undefined,
-                show: true,
-            },
-            y: {
-                formatter: undefined,
-                title: {
-                    formatter: (seriesName) => seriesName,
-                },
-            },
-            z: {
-                formatter: undefined,
-                title: 'Size: ',
-            },
-        },
-        xaxis: {
-            categories: coinTimestamp,
-        },
-    };
+    // const options: ApexOptions = {
+    //     chart: {
+    //         animations: {
+    //             animateGradually: {
+    //                 delay: 150,
+    //                 enabled: true,
+    //             },
+    //             dynamicAnimation: {
+    //                 enabled: true,
+    //                 speed: 350,
+    //             },
+    //             easing: 'easeinout',
+    //             enabled: true,
+    //             speed: 800,
+    //         },
+    //         background: '#fff',
+    //         height: 600,
+    //         zoom: {
+    //             enabled: true,
+    //         },
+    //     },
+    //     dataLabels: {
+    //         enabled: false,
+    //     },
+    //     fill: {
+    //         colors: ['#2E93fA', '#66DA26', '#546E7A', '#E91E63', '#FF9800'],
+    //     },
+    //     grid: {
+    //         borderColor: '#90A4AE',
+    //         column: {
+    //             colors: undefined,
+    //             opacity: 0.5,
+    //         },
+    //         padding: {
+    //             bottom: -10,
+    //             left: 10,
+    //             right: 10,
+    //             top: 0,
+    //         },
+    //         position: 'back',
+    //         row: {
+    //             colors: undefined,
+    //             opacity: 0.5,
+    //         },
+    //         show: true,
+    //         strokeDashArray: 0,
+    //         xaxis: {
+    //             lines: {
+    //                 show: false,
+    //             },
+    //         },
+    //         yaxis: {
+    //             lines: {
+    //                 show: false,
+    //             },
+    //         },
+    //     },
+    //     markers: {
+    //         colors: undefined,
+    //         discrete: [],
+    //         fillOpacity: 1,
+    //         hover: {
+    //             size: undefined,
+    //             sizeOffset: 3,
+    //         },
+    //         offsetX: 0,
+    //         offsetY: 0,
+    //         onClick: undefined,
+    //         onDblClick: undefined,
+    //         radius: 2,
+    //         shape: 'circle',
+    //         showNullDataPoints: true,
+    //         size: 0,
+    //         strokeColors: '#fff',
+    //         strokeDashArray: 0,
+    //         strokeOpacity: 0.9,
+    //         strokeWidth: 2,
+    //     },
+    //     stroke: {
+    //         curve: 'straight',
+    //     },
+    //     tooltip: {
+    //         custom: undefined,
+    //         enabled: true,
+    //         enabledOnSeries: undefined,
+    //         fillSeriesColor: false,
+    //         fixed: {
+    //             enabled: false,
+    //             offsetX: 0,
+    //             offsetY: 0,
+    //             position: 'topRight',
+    //         },
+    //         followCursor: false,
+    //         intersect: false,
+    //         inverseOrder: false,
+    //         items: {
+    //             display: 'flex',
+    //         },
+    //         marker: {
+    //             show: true,
+    //         },
+    //         onDatasetHover: {
+    //             highlightDataSeries: false,
+    //         },
+    //         shared: true,
+    //         style: {
+    //             fontFamily: undefined,
+    //             fontSize: '16px',
+    //         },
+    //         x: {
+    //             format: 'dd MMM',
+    //             formatter: undefined,
+    //             show: true,
+    //         },
+    //         y: {
+    //             formatter: undefined,
+    //             title: {
+    //                 formatter: (seriesName) => seriesName,
+    //             },
+    //         },
+    //         z: {
+    //             formatter: undefined,
+    //             title: 'Size: ',
+    //         },
+    //     },
+    //     xaxis: {
+    //         categories: coinTimestamp,
+    //     },
+    // };
 
-    const series = [
-        {
-            data: coinPrice,
-            name: 'prices',
-        },
-    ];
+    // const series = [
+    //     {
+    //         data: coinPrice,
+    //         name: 'prices',
+    //     },
+    // ];
 
     if (!details) return <Spinner />;
     return (
@@ -259,16 +257,14 @@ const SingleCoinDetails: React.FC<IProps> = ({ data }: IProps) => {
                     View value statistics, market cap and supply
                 </p>
             </section>
-
-            {/* COIN CHART */}
+            {/* COIN CHART
             <section>
                 {isFetching && isLoading ? (
                     <Loading />
                 ) : (
-                    <ApexCharts type="area" height="350" options={options} series={series} />
+                    <ApexCharts type="area" height="600" options={options} series={series} />
                 )}
-            </section>
-
+            </section> */}
             {/* COIN OVERVIEW */}
             <section className="grid grid-cols-1 lg:grid-cols-2 gap-12">
                 <div className="space-y-10">
@@ -319,7 +315,6 @@ const SingleCoinDetails: React.FC<IProps> = ({ data }: IProps) => {
                     </main>
                 </div>
             </section>
-
             {/* COIN DESC */}
             <section className="grid grid-cols-1 lg:grid-cols-2 gap-12">
                 <div>
