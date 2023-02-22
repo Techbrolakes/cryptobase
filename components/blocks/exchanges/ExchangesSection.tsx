2a24d43859b17b43433090e8590db21cbd7efda5
Techbrolakes <lekandar11@gmail.com>
2023-02-22 12:32:47 +0100

    feat: added search functionality for exchanges

--- components/blocks/exchanges/ExchangesSection.tsx
+++ components/blocks/exchanges/ExchangesSection.tsx
@@ -12,10 +12,17 @@ import { useRouter } from 'next/router';
 const { CLIENT_ROUTES } = config;
 const ExchangesSection: React.FC = () => {
     const { data, isFetching } = useGetExchangesQuery(10);
+    const [searchQuery, setSearchQuery] = useState('');
     const router = useRouter();
     const coinPerRow = 9;
     const [next, setNext] = useState(coinPerRow);
     const [loading, setLoading] = useState(false);
+    const handleSearchInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setSearchQuery(event.target.value);
+    };
+    const filteredExchanges = data.filter((exchange) =>
+        exchange.name.toLowerCase().includes(searchQuery.toLowerCase()),
+    );
 
     const handleLoadMore = () => {
         setLoading(true);
@@ -30,7 +37,13 @@ const ExchangesSection: React.FC = () => {
     return (
         <section className="p-1 lg:p-6 space-y-12 mb-20 lg:mb-20">
             <div className="w-2/3 lg:w-1/2 mx-auto">
-                <Input name="" variant="filled" placeholder="Search Exchanges" />
+                <Input
+                    name="searchExchanges"
+                    variant="filled"
+                    placeholder="Search Exchanges"
+                    value={searchQuery}
+                    onChange={handleSearchInputChange}
+                />
             </div>
             <h1 className="cb-text text-center">
                 A cryptocurrency exchange, or a digital currency exchange, is a business that allows customers to trade
@@ -39,30 +52,35 @@ const ExchangesSection: React.FC = () => {
             </h1>
             <div>{!data && <Loading />}</div>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 lg:gap-4">
-                {data?.slice(0, next).map(({ name, image, trust_score_rank, country, year_established, id }) => (
-                    <Box
-                        className="w-full lg:w-[330px] shadow-md space-y-12 p-8 cursor-pointer"
-                        key={name}
-                        onClick={() => router.push(CLIENT_ROUTES.exchangeDetails.replace('%id%', id))}
-                    >
-                        <section className="flex justify-between items-center">
-                            <article className="flex gap-3 items-center">
-                                <span className="cb-text">{trust_score_rank}</span>
-                                <span className="cb-text">{name}</span>
-                            </article>
-                            <Avatar src={image} size={'sm'} />
-                        </section>
+                {filteredExchanges
+                    ?.slice(0, next)
+                    .map(({ name, image, trust_score_rank, country, year_established, id }) => (
+                        <Box
+                            className="w-full lg:w-[330px] shadow-md space-y-12 p-8 cursor-pointer"
+                            key={name}
+                            onClick={() => router.push(CLIENT_ROUTES.exchangeDetails.replace('%id%', id))}
+                        >
+                            <section className="flex justify-between items-center">
+                                <article className="flex gap-3 items-center">
+                                    <span className="cb-text">{trust_score_rank}</span>
+                                    <span className="cb-text">{name}</span>
+                                </article>
+                                <Avatar src={image} size={'sm'} />
+                            </section>
 
-                        <section className="space-y-4">
-                            <p className="cb-text tracking-wide"> Country - {country} </p>
-                            <p className="cb-text tracking-wide"> Worldwide Rank - {getOrdinal(trust_score_rank)}</p>
-                            <p className="cb-text tracking-wider"> Year Established - {year_established} </p>
-                        </section>
-                    </Box>
-                ))}
+                            <section className="space-y-4">
+                                <p className="cb-text tracking-wide"> Country - {country} </p>
+                                <p className="cb-text tracking-wide">
+                                    {' '}
+                                    Worldwide Rank - {getOrdinal(trust_score_rank)}
+                                </p>
+                                <p className="cb-text tracking-wider"> Year Established - {year_established} </p>
+                            </section>
+                        </Box>
+                    ))}
             </div>
             <div className="flex justify-center">{loading && <Lottie animationData={bitcoin} className="w-24" />}</div>{' '}
-            {next < data?.length && (
+            {next < filteredExchanges?.length && (
                 <div className="flex justify-center">
                     <Button onClick={handleLoadMore}>Load More Exchanges</Button>
                 </div>
